7e05429a724f80ecb113a5f90bfe8caf11df0092
Szymon Oledzki <spoledzki@gmail.com>
2022-12-21 10:21:16 +0100

    Initial commit :)

---
+++ Gra.cpp
@@ -0,0 +1,57 @@
+//
+// Created by Szymon 20.12.2022.
+//
+
+#include "Gra.h"
+
+
+Gra::Gra() = default;
+
+Gra::~Gra() = default;
+
+void Gra::WhereIsLegal(int k) {
+    int i=0;
+    while(Plansza[k][i] != 0) i++;
+    if(i<6) {
+        legalny_ruch = true;
+        tu_wstaw = i;
+    }
+    else legalny_ruch = false;
+}
+
+int Gra::Rezultat() {
+    return 0;
+}
+
+void Gra::WykonajRuch() {
+    if(0 != tury % 2){
+        kolor_zetonu = 1;
+    }
+    else{
+        kolor_zetonu = 2;
+    }
+    std::cout << "Tura gracza "<< kolor_zetonu <<" - którą kolumnę wybierasz?";
+    std::cin >> kolumna;
+    WhereIsLegal(kolumna-1);
+    if(legalny_ruch){
+        Plansza[kolumna-1][tu_wstaw] = kolor_zetonu;
+        tury++;
+    }
+    else{
+        std::cout << "Ten ruch jest nielegalny, wybierz inną kolumnę." << std::endl;
+        WykonajRuch();
+    }
+}
+
+void Gra::DrukujPlansze() {
+    for(int j=5; j>=0 ; j--){
+        std::cout << "| ";
+        for (int i = 0; i < 7; i++)
+            std::cout << Plansza[i][j] << " ";
+        std::cout << "|" << std::endl;
+    }
+    std::cout << "=================" << std::endl;
+}
+
+
+
