4e1397eb9fe453af743771e439cf6175f535f987
Szymon <62554479+Fioletuwa@users.noreply.github.com>
2023-01-11 16:04:23 +0100

    Zmiana deklaracji roundCount.

--- Gra.h
+++ Gra.h
@@ -43,7 +43,7 @@ private:
     int tokenColor = 0;
     bool MoveLegal = false;
     int rowToInsert = 0;
-    int roundCount;
+    int roundCount = 0;
     int lastPickCol = 0;
     bool isGameOver = false;
     int winningToken = 0;
